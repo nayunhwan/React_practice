ec5ef0ad04dd7db93707666685ebbb76b9c30e56
nayunhwan <kbk9288@gmail.com>
2016-12-14 14:43:47 +0900

    Add Chapter04

---
+++ Chapter04/main.js
@@ -0,0 +1,14 @@
+// This is Chapter04 Example, It explain to excute function per 1 sec
+
+function tic(){
+  const element =(
+    <div>
+      <h1>Hello Tic Tak Tok</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+
+  ReactDOM.render(element, document.getElementById("root"));
+}
+
+setInterval(tic, 1000);
