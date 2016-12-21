d3abc5ed5b92de1e039fc3f0e255c9b67ae476f3
nayunhwan <kbk9288@gmail.com>
2016-12-21 20:04:19 +0900

    Event Handling

--- Chapter05/main.js
+++ Chapter05/main.js
@@ -1,37 +1,43 @@
-// This is Chapter04 Example, It explains that how to handle event.
+// This is Chapter05 Example, It explains that how to handle event.
 
 class Element extends React.Component{
   constructor(props) {
     super(props);
 
-    this.state = {value: 'jnjkn'};
+    // Only React
+    this.state = {
+        value: ''
+    };
+
+    // bind를 하지 않으면 메소드에서 this가 뭔지 모른다.
     this.handleChange = this.handleChange.bind(this);
     console.log(props)
   }
 
-  // handleChange(event) {
-  //   this.setState({value: event.target.value});
-  // }
-
-  // var handleChange = (event) => ({value: event.target.value})
-
   handleChange(event){
-    // alert('hello');
-    console.log(event.target.value);
-    this.setState({value : event.target.value})
+      // console.log(event);
+      // console.log(event.target.value);
+      this.setState({value : event.target.value})
+  }
+
+  keyDown(event) {
+      console.log(event.key);
   }
 
   render(){
     return(
       <div>
-        <input type="text" value={this.state.value} onChange={this.handleChange} />
-        <input type="text" value={this.state.value}/>
+        <input type="text"  value={this.state.value} onChange={this.handleChange} onKeyDown={this.keyDown}/>
+        {/*<input type="text" value={this.state.value}/>*/}
         <h1>{this.state.value}</h1>
         <h1>오직</h1>
-
+        <Sub
+            input={this.state.value}
+        />
       </div>
     )
   }
 }
+
 
 ReactDOM.render(<Element/>, document.getElementById("root"));
