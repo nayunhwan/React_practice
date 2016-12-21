753e58b4494f1e4266459f551b7a5c7e6f015574
nayunhwan <kbk9288@gmail.com>
2016-12-21 20:22:01 +0900

    Add Chapter6

---
+++ Chapter06/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by nayunhwan on 2016. 12. 21..
+ */
+
+class DefaultComponent extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <h1>hello world</h1>
+
+        )
+    }
+
+}
+ReactDOM.render(<DefaultComponent/>, document.getElementById("container"));
