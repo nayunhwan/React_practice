a1e61498c77fc4165a81e98d16626778149bf057
nayunhwan <kbk9288@gmail.com>
2016-12-22 15:42:35 +0900

    Add TodoList.js

---
+++ Chapter08/src/Components/TodoList.js
@@ -0,0 +1,28 @@
+/**
+ * Created by nayunhwan on 2016. 12. 22..
+ */
+
+import React from 'react';
+import Todo from './Todo';
+
+export default class TodoList extends React.Component{
+    render(){
+        return(
+            <ul>
+                {this.props.todo.map((todo, index))}
+                <Todo
+                    {...Todo}
+                    key={index}
+                    onClick={() => this.props.onTodoClick(index)}
+                />
+            </ul>
+        )
+    }
+}
+TodoList.propTypes = {
+    onTodoClick: PropTypes.func.isRequired,
+    todos: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    completed: PropTypes.bool.isRequired
+}).isRequired).isRequired
+};
