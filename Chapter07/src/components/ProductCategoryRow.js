8720fdd74e4f884083abe9f403c230affaf80e34
nayunhwan <kbk9288@gmail.com>
2016-12-23 10:30:36 +0900

    Edit Chapter07

--- Chapter07/src/components/ProductCategoryRow.js
+++ Chapter07/src/components/ProductCategoryRow.js
@@ -1,3 +1,15 @@
 /**
  * Created by nayunhwan on 2016. 12. 22..
  */
+
+import React, { Component } from 'react';
+
+export default class ProductCategoryRow extends Component{
+    render(){
+        return(
+            <tr>
+                <th colSpan="2">{this.props.category}</th>
+            </tr>
+        )
+    }
+}
--- Chapter07/src/components/SearchBar.js
+++ Chapter07/src/components/SearchBar.js
@@ -1,3 +1,36 @@
 /**
  * Created by nayunhwan on 2016. 12. 22..
  */
+
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component{
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(){
+        this.props.onUserInput(
+            this.filterTextInput.value,
+            this.inStockOnlyInput.checked
+        )
+    }
+
+    render(){
+        return(
+            <form>
+                <input type="text" placeholder="Search..."
+                       value={this.props.filterTextInput} ref={(input) => this.filterTextInput = input} onChange={this.handleChange}
+                />
+                <p>
+                    <input type="checkbox"
+                           checked={this.props.inStockOnly} ref={(input) => this.inStockOnlyInput = input} onChange={this.handleChange}
+                    />
+                    {' '}
+                    Only show products in stock
+                </p>
+            </form>
+        )
+    }
+}
