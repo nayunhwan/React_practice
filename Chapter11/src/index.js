5073612f2037e5f1ad5dbfd17897c828aee6a335
nayunhwan <kbk9288@gmail.com>
2016-12-23 11:07:37 +0900

    Install Redux

--- Chapter11/src/index.js
+++ Chapter11/src/index.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+/**
+ * Created by nayunhwan on 2016. 12. 23..
+ */
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+
